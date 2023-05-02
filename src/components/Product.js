39d8ca569ab0bc7ec75d9d77a6ec9ed9fe55d2d0
Kalai Bharath <kalaicbi28@gmail.com>
2023-05-03 01:02:21 +0530

    fixed bug

--- src/components/Product.js
+++ src/components/Product.js
@@ -17,13 +17,13 @@ export default function Product({id,title,price,description,category,image}) {
         <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
         <Image src={image} width={200} height={200} style={{objectFit:'contain',alignSelf:"center"}}/>
         <h4 className="my-4">{title}</h4>
-        <div  suppressHydrationWarning={true} className="flex">
+        {/* <div  suppressHydrationWarning={true} className="flex">
             {Array(rating)
             .fill().map((_,i) =>(
        <StarIcon className="h-5 text-yellow-300"/>
             ))}
           
-        </div>
+        </div> */}
         <p className="text-xs my-2 line-clamp-2">{description}</p>
         <div className="my-2">
             <Currency quantity={price} currency='usd'/>
